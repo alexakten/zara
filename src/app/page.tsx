53e717417a76dc3ad086acb4bfb14054ba56fb42
Alex <alex.akten@outlook.com>
2024-02-29 16:51:46 +0100

    zara init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,106 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState, useRef, useEffect, useMemo } from "react";
+import Link from "next/link";
+import Navbar from "./components/Navbar";
+
+export default function LandingPage() {
+  const textPhrases = ["your website.", "social media.", "marketing."];
+
+  const [currentTextPhrase, setCurrentTextPhrase] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [textIndex, setTextIndex] = useState(0);
+  const typingSpeed = 65; // Adjust typing speed
+
+  useEffect(() => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+
+    if (isDeleting) {
+      if (currentTextPhrase === "") {
+        setIsDeleting(false);
+        setTextIndex((index) => (index + 1) % textPhrases.length);
+        timeoutId = setTimeout(() => {
+          setCurrentTextPhrase(
+            textPhrases[(textIndex + 1) % textPhrases.length].substring(0, 1)
+          );
+        }, 2000); // Wait half a second before typing new phrase
+      } else {
+        timeoutId = setTimeout(() => {
+          setCurrentTextPhrase(
+            currentTextPhrase.substring(0, currentTextPhrase.length - 1)
+          );
+        }, typingSpeed);
+      }
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [currentTextPhrase, isDeleting, textIndex, textPhrases]);
+
+  useEffect(() => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+
+    if (!isDeleting && currentTextPhrase !== textPhrases[textIndex]) {
+      timeoutId = setTimeout(() => {
+        setCurrentTextPhrase(
+          textPhrases[textIndex].substring(0, currentTextPhrase.length + 1)
+        );
+      }, typingSpeed);
+    } else if (currentTextPhrase === textPhrases[textIndex]) {
+      timeoutId = setTimeout(() => {
+        setIsDeleting(true);
+      }, 2000); // Wait 2 seconds before starting to delete
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [currentTextPhrase, isDeleting, textIndex, textPhrases]);
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center justify-center bg-zinc-100 px-4">
+      <Navbar />
+      {/* Hero Section */}
+      <section className="flex h-screen w-screen items-center justify-center px-4">
+        <div
+          className="flex  flex-col items-center gap-6 text-center text-gray-900"
+          style={{ maxWidth: 880 }}
+        >
+          <div className="rounded-full border border-zinc-300 bg-white  px-4 py-1 text-zinc-400">
+            v1.0 under construction <span className="text-[12px]">🚧</span>
+          </div>
+          <div>
+            <h1 className="gray-shadow text-5xl font-bold tracking-tighter sm:text-8xl">
+              Video testimonials <br /> for{" "}
+              <span className="black-shadow">{currentTextPhrase}</span>
+              <span className="typing-cursor">|</span>
+            </h1>
+          </div>
+
+          <div className="text-md xs:text-xl" style={{ maxWidth: 624 }}>
+            <p>
+              Introducing the simplest way to collect & publish video
+              testimonials. Start boosting your business with social proof
+              today.
+            </p>
+          </div>
+          <div className="flex flex-col items-center gap-4 xs:flex-row">
+            <Link href={"/form"}>
+              <button
+                type="button"
+                className="box-shadow flex h-12 items-center justify-center rounded-lg border border-black bg-white px-4 font-medium text-black hover:bg-purple hover:text-white"
+              >
+                Try demo 🌵
+              </button>
+            </Link>
+          </div>
+          <p className="text-zinc-400">
+            {lang === "en" ? "It's so easy! " : "Det är så enkelt! 🌵"}
+          </p>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* <section className="flex w-screen pb-40 items-center justify-center px-4">
+        <Record question="How did you hear about us?"/>
+      </section> */}
     </main>
   );
 }
